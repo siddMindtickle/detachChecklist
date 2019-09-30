39de26b156e92b193c354b27244a59fb0eb97e54
siddharth.patel <siddharth.patel@mindtickle.com>
2019-09-30 15:18:40 +0530

    cahnged root from Root

--- app/index.js
+++ app/index.js
@@ -1,5 +1,5 @@
 import React from "react";
 import bootstrap from "@mt-ui-core";
-import Root from "@components/Root";
+import Root from "@components/root";
 
 bootstrap("root", { Application: <Root /> });
