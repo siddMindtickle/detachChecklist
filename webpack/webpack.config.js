3a980d1844970a4384a11f9100ebc34011a62468
siddharth.patel <siddharth.patel@mindtickle.com>
2019-10-15 16:05:49 +0530

    removed @icon from webpack config

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -65,11 +65,6 @@ config.resolve = {
     "@mixpanel": resolve(__dirname, "../app/mixpanel"),
     "@containers": resolve(__dirname, "../app/containers"),
     "@uielements": resolve(__dirname, "../app/components/uielements"),
-    "@ant-design/icons": resolve(
-     __dirname,
-     "../node_modules",
-     "@mindtickle/mt-ui-components/src/Icon/antIcons.js"
-   )
   }
 };
 
