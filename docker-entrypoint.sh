3325ff1513ae260fe344b5aedc09779d4cb5b320
siddharth.patel <siddharth.patel@mindtickle.com>
2019-11-11 17:37:05 +0530

    performance-tool changes

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -3,7 +3,7 @@
 set -ex
 
 #echo environment variables
-env
+env = $TRACK
 
 # deploy allaboard assets
 public_path="//${ASSETS_BUCKET}/${APPLICATION}/${TRACK}"
@@ -18,8 +18,29 @@ cd /app/dist
 aws s3 sync --acl public-read --cache-control max-age=25012345 assests-ui/ $s3Url/assests-ui/
 
 #static
+
 aws s3 cp --acl public-read --cache-control max-age=0,no-cache,no-store,must-revalidate index.html $s3Url/index.html
 aws s3 cp --acl public-read favicon.ico $s3Url/favicon.ico
+
+
+utcTime=$(date +%s)
+istTime=$(($utcTime+19800))
+dates=$(date -d @$istTime +%D-%T)
+jobName="Build_triggered_checklist_${dates}"
+
+
+if[[env == 'prod']]
+  then env=''
+else
+  env=".$env."
+fi
+
+endpoint="http://webpagetest${env}mindtickle.com/app/testChecklist"
+
+sleep 5s
+
+curl -H "Accept: application/json" -H "Content-type: application/json" -X POST -d '{"jobName":"'"$jobName"'","dataEndpoint":"'"$endpoint"'"}'  "http://webpagetest${env}mindtickle.com/app/createInternalJob"
+
 
 
 set +ex
